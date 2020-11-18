bcfa6a6e92ccf9548f9743a5fb6d8186c9c58164
rizkyfaza20 <rizkyfaza61@gmail.com>
2020-11-18 15:29:07 +0700

    Changes

--- connect-to-server.py
+++ connect-to-server.py
@@ -3,7 +3,11 @@
 import requests
 import subprocess
 import os
+import datetime
 
+nama_file = 'report_file.xlsx'
+timestamp = datetime.datetime.now()
+t = timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute, timestamp.second
 
 def connect_server(credentials):
     """
@@ -17,10 +21,10 @@ def connect_server(credentials):
 
     url_report = 'http://192.168.6.127:81/export.aspx?export=xls&det=Web50getdevicebytype&title=All+Windows+assets&@devicetype=-1'
     r = requests.get(url_report)
-    
 
     """
     Line above used for crawl and download data from the specific URL
-
     """
-    with open() as target:
+    with open('Web50getdevicebytype.xlsx','wb') as f:
+        f.write(r.content)
+        os.rename('Web50getdevicebytype.xlsx',nama_file + '_' + '-'.join(t))
