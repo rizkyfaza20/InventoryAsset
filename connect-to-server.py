41e37a432bd17b40d7154991e99ee9c734395537
rizkyfaza20 <rizkyfaza61@gmail.com>
2020-11-18 16:52:06 +0700

    Added download-report

--- connect-to-server.py
+++ connect-to-server.py
@@ -1,13 +1,8 @@
-#!/bin/usr/python
+#!/bin/usr/python3
 
 import requests
-import subprocess
 import os
 import datetime
-
-nama_file = 'report_file.xlsx'
-timestamp = datetime.datetime.now()
-t = timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute, timestamp.second
 
 def connect_server(credentials):
     """
@@ -19,12 +14,3 @@ def connect_server(credentials):
     # In this session you've been logged in to Web Server
     r = requests.post(url, data=credentials)
 
-    url_report = 'http://192.168.6.127:81/export.aspx?export=xls&det=Web50getdevicebytype&title=All+Windows+assets&@devicetype=-1'
-    r = requests.get(url_report)
-
-    """
-    Line above used for crawl and download data from the specific URL
-    """
-    with open('Web50getdevicebytype.xlsx','wb') as f:
-        f.write(r.content)
-        os.rename('Web50getdevicebytype.xlsx',nama_file + '_' + '-'.join(t))
